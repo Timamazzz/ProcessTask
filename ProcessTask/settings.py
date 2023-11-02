5fe3ac06cb987bd58b6fc0b3dcacbf73a0fe4f15
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-02 11:49:09 +0300

    fix

--- ProcessTask/settings.py
+++ ProcessTask/settings.py
@@ -155,6 +155,10 @@ USE_TZ = True
 MEDIA_URL = 'media/'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "build", "static"),
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
