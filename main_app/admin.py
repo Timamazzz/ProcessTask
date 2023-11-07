b769f5510d97ee4c1c3315561d0ca2b30686a9bc
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-07 15:08:46 +0300

    fix add user

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from .models import CustomUser, LifeSituation, Process, Service, Organization
+from django.contrib.auth.admin import UserAdmin
 
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
 
 
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'patronymic', 'organization')
     search_fields = ('email', 'first_name', 'last_name', 'patronymic', 'organization__name')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'date_joined', 'organization')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'patronymic', 'organization')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 class LifeSituationAdmin(admin.ModelAdmin):
