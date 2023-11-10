adb9dce59ff516a9f8b67b1c20204a923665475e
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-10 16:53:56 +0300

    fix

--- ProcessTask/settings.py
+++ ProcessTask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u*dj5xyj4#dk=4y!gw@@hou#zf=la6!5($ucgn$fzw&j8@@^-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
