30eeda6036ffaeaad3459b64cb91fef25a5bc1ac
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-19 20:36:20 +0300

    fix

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,10 +1,60 @@
+import openpyxl
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import CustomUser, LifeSituation, Process, Service, Organization
 from django.contrib.auth.admin import UserAdmin
+from openpyxl.writer.excel import save_virtual_workbook
 
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
+
+    actions = ['export_to_excel']
+
+    def export_to_excel(self, request, queryset):
+        # Create a new Excel workbook and add worksheets for each model
+        wb = openpyxl.Workbook()
+        life_situation_ws = wb.create_sheet(title='LifeSituation')
+        service_ws = wb.create_sheet(title='Service')
+        process_ws = wb.create_sheet(title='Process')
+
+        # Add headers to each worksheet
+        life_situation_ws.append(['Name', 'Identifier', 'User'])
+        service_ws.append(['Name', 'Service Type', 'Regulating Act', 'Life Situation', 'Identifier', 'User'])
+        process_ws.append(['Name', 'Service', 'Status', 'Internal Client', 'External Client',
+                           'Responsible Authority', 'Department', 'Digital Format', 'Non-Digital Format',
+                           'Digital Format Link', 'Identifier', 'Client Value', 'Input Data', 'Output Data',
+                           'Related Processes', 'User', 'Group'])
+
+        # Populate each worksheet with data from the selected Organization
+        for organization in queryset:
+            life_situations = LifeSituation.objects.filter(user__organization=organization)
+            services = Service.objects.filter(user__organization=organization)
+            processes = Process.objects.filter(user__organization=organization)
+
+            for life_situation in life_situations:
+                life_situation_ws.append([life_situation.name, life_situation.identifier, life_situation.user])
+
+            for service in services:
+                service_ws.append([service.name, service.service_type, service.regulating_act, service.lifesituation,
+                                   service.identifier, service.user])
+
+            for process in processes:
+                process_ws.append([process.name, process.service, process.status, process.is_internal_client,
+                                   process.is_external_client, process.responsible_authority, process.department,
+                                   process.is_digital_format, process.is_non_digital_format,
+                                   process.digital_format_link,
+                                   process.identifier, process.client_value, process.input_data, process.output_data,
+                                   process.related_processes, process.user, process.group])
+
+        # Create a response with the Excel file
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=exported_data.xlsx'
+        response.write(save_virtual_workbook(wb))
+        return response
+
+    export_to_excel.short_description = "Export selected organization's data to Excel"
 
 
 class CustomUserAdmin(UserAdmin):
